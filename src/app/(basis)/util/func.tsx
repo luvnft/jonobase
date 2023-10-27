af61ff4c41aef8e40a30eff4d9b65909ff90d040
Joncoded <joncoded@gmail.com>
2023-10-27 03:22:46 -0400

    adding helpful comments

--- src/app/(basis)/util/func.tsx
+++ src/app/(basis)/util/func.tsx
@@ -10,19 +10,23 @@ export const getImageURL = (collectionId: string, id: string, icon: string) => {
 }  
 
 export const getFormattedDate = (oldDate: any) => {
+  // YYYY-MM-DD
   return oldDate.substring(0, 10)
 }
 
 export const getFormattedDateTime = (oldDate: any, seconds: boolean = false) => {
+  // YYYY-MM-DD hh:mm(:ss)
   const ending = seconds ? 19 : 16
   return oldDate.substring(0, ending)
 }
 
 export const getFormattedYearMonth = (oldDate: any) => {
+  // YYYY-MM
   return oldDate.substring(0, 7)
 }
 
 export const getFormattedTime = (oldDate: any, seconds: boolean = false) => {
+  // hh:mm or hh:mm:ss
   const ending = seconds ? 19 : 16
   return oldDate.substring(11, ending)
 }
