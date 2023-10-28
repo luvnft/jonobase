794dd49b931a7b6f977bd3b7443a055978306ad0
Joncoded <joncoded@gmail.com>
2023-10-28 15:52:00 -0400

    moving loading spinner

--- src/app/(basis)/util/load-spin.tsx
+++ src/app/(basis)/util/load-spin.tsx
@@ -8,7 +8,7 @@ export async function Load() {
   return (
     <section className={`flex flex-row w-full h-[50vh] justify-center items-center`}>
       <div>
-        <Image src={`/load-spin.gif?v=${Date.now()}`} alt={lang.loading} height={64} width={64} />          
+        <Image src={`/images/load-spin.gif?v=${Date.now()}`} alt={lang.loading} height={64} width={64} />          
       </div>
     </section>   
   )
