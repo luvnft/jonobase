86448e4cb9ab7f6db883ec11a353db48616bf209
Joncoded <joncoded@gmail.com>
2023-10-28 16:08:07 -0400

    updating post-head layout

--- src/app/[langs]/[type]/(post)/(libs)/post-head.tsx
+++ src/app/[langs]/[type]/(post)/(libs)/post-head.tsx
@@ -19,35 +19,34 @@ export default async function PostHead({lang, post}: any) {
         <div className={`font-sans w-full`}>
 
           <h2 className={`
-            text-4xl md:text-6xl mb-5 uppercase
+            text-2xl md:text-6xl mb-5 uppercase
           `}>{post.emoji} {post.title}</h2>
 
-          <Line className={`
-            text-lg md:text-xl my-5
-          `}>
-            {post.url && 
-              <Span>                
-                <Link 
-                  className={`button`} 
-                  href={post.url} 
-                  target={post.url_newtab ? '_blank' : ''}
-                >
-                  {lang.visit_link}
-                </Link>                
-              </Span>
-            }
-            <Span>{post.summary}</Span>
-          </Line>
+          <Line>{post.summary}</Line>
 
+          <Line className={`
+            text-sm md:text-xl 
+          `}>
+            {post.url &&               
+              <Link 
+                className={`button`} 
+                href={post.url} 
+                target={post.url_newtab ? '_blank' : ''}
+              >
+                {lang.visit_link}
+              </Link>                              
+            }
+          </Line>
+              
         </div>
 
         <div className={`
-          font-sans w-full text-lg md:text-xl text-gray-500 dark:text-gray-300 mt-2 mb-2 text-right       
+          font-sans w-full text-sm md:text-xl text-gray-500 dark:text-gray-300 mt-2 mb-2 text-right       
         `}>
               
           {post.updated !== undefined && 
             <Line>                        
-              <Span className={`text-md mb-0 pb-0`}>              
+              <Span>              
                 {`${lang.last_updated} ${getFormattedDate(post.updated)}`}              
               </Span>          
             </Line>
@@ -57,7 +56,7 @@ export default async function PostHead({lang, post}: any) {
         
           {post.backdated !== undefined && 
             <Line>            
-              <Span className={`text-md mt-0 pt-0`}>
+              <Span>
                 {`${lang.originally_created} ${getFormattedDate(post.backdated)}`} 
               </Span>
             </Line>
