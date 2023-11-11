a2bdf2ed486ce5f8fc1fd08f9d3ded97e7fd69a4
Joncoded <joncoded@gmail.com>
2023-11-11 15:43:22 -0500

    adjusting dark-mode backgrounds

--- src/app/(basis)/view/view-show.tsx
+++ src/app/(basis)/view/view-show.tsx
@@ -29,7 +29,7 @@ export default async function ViewShow({lang, takeView}: any) {
         py-10 drop-shadow-lg 
         bg-${view.bg_color}${view.bg_color_intensity ? `-${view.bg_color_intensity}` : ``} 
         text-${view.text_color}
-        dark:bg-${view.bg_color}${view.bg_color_intensity ? `-900` : ``}
+        dark:bg-${view.bg_color}${view.bg_color_intensity ? `-800` : ``}
         dark:text-white
       `}>
 
